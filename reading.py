01c5be5be18334d2db252cdf7400543f49ceb3f2
Andrea Turek <andreaturek@wirelessprv-10-193-62-225.near.illinois.edu>
2024-10-08 22:44:16 -0500

    committing text parse fastq

---
+++ reading.py
@@ -0,0 +1,27 @@
+
+
+# Open the FASTQ file for reading
+fastq = open('SRR28797574_1.fastq', 'r')
+
+genome = open('genome.txt', 'w')
+
+count = 0
+lineCount = 0  
+while count < 10:
+  line = fastq.readline() 
+  if not line:
+    break 
+
+  if lineCount % 4 == 1:  
+    genome.write(line.strip() + "\n")  
+    count += 1
+
+  lineCount += 1  
+
+fastq.close()
+
+
+
+
+
+
