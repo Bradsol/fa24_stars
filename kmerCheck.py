afc4e9312b351fc099fd4e09df933c5af3f659ea
amturek2 <amturek2@illinois.edu>
2024-10-31 13:37:32 -0500

    updating Kmer chekcing files

--- kmerCheck.py
+++ kmerCheck.py
@@ -1,3 +1,12 @@
+import sys
+
+if len(sys.argv) < 4:
+    print("Usage: python file.py <signal_gene_file> <genome_file> <output_kmer_file>")
+    sys.exit(1)
+
+signal_gene_input = sys.argv[1]
+genome_input = sys.argv[2]
+output_kmer_input = sys.argv[3]
 
 def create_kmers(sequence, k): 
     kmers = []
@@ -11,52 +20,47 @@ def kmer_count(kmers, text, k):
   for kmer in kmers: #iterates through kmer list, initializing every count to 0 
     kmer_counts[kmer] = 0
 
-  for i in range(len(text) - k + 1): 
-    subsequence = text[i:i+k]
-    if subsequence in kmer_counts:
+  for i in range(len(text) - k + 1): # goes through every possible substring of length of kmer
+    subsequence = text[i:i+k] 
+    if subsequence in kmer_counts: # if that substring is equal to a kmer in the list, increment the kmer count 
       kmer_counts[subsequence] += 1
   
   return kmer_counts
 
-#open file 
-
-with open('KRAS.txt', 'r') as kras_file:
+ 
+#FILE WE WANT KMERS OF
+with open(signal_gene_input, 'r') as kras_file:
     text_kras = kras_file.read().replace('\n', '')  #removes the \n 
 
 
-
-with open('cleanGenome.txt', 'r') as cleanGenome:
-    k = 50  # Length of k-mers
+#FILE WE SEARCH FOR KMERS IN 
+with open(genome_input, 'r') as cleanGenome:
+    k = 50  # specified length of Kmer
     num = 1
     
-    # Create k-mers from the KRAS sequence
+    # creating kmer of kras 
     kmers = create_kmers(text_kras, k)
     total_kmer_counts = {kmer: 0 for kmer in kmers}
 
-    # Iterate over lines in cleanGenome.txt
+    # iterate over lines in cleanGenome.txt
     for line in cleanGenome: 
-      #print(line)
-      if num > 100000:  
-          break
+      #if num > 200:  
+          #break
 
-        # Count k-mers in the current line of cleanGenome
-      kmer_count_dict = kmer_count(kmers, line.strip(), k)  # Strip whitespace/newline characters
+      # count k-mers in the current line of cleanGenome
+      kmer_count_dict = kmer_count(kmers, line.strip(), k) 
       #print(f"Line {num}: {kmer_count_dict}")
-      num += 1
-      print(num)
 
+      #add each line kmer count a total dictionary 
       for kmer, count in kmer_count_dict.items():
             total_kmer_counts[kmer] += count
+      
 
-# Find the k-mer that occurred the most often
-most_frequent_kmer = max(total_kmer_counts, key=total_kmer_counts.get)
-most_frequent_count = total_kmer_counts[most_frequent_kmer]
-
-# Print the most frequent k-mer and its count
-print(f"The most frequent k-mer is: {most_frequent_kmer} with a count of {most_frequent_count}")
+      num += 1
+      print(num) #track the lines
 
 
-
-
-
-
+#OPEN OUTPUT FILE and write each kmer and count to the file 
+with open(output_kmer_input, 'w') as output_file:
+    for kmer, count in total_kmer_counts.items():
+        output_file.write(f"{kmer}: {count}\n")
